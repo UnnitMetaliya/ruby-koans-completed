168d1b0cae3f60f56add30c718a2adc21918537d
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-02-15 19:19:07 -0500

    completed about_triangle_project_2 161/282

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,8 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError if a<=0 or b<=0 or c<=0
+  raise TriangleError if a+b<=c or b+c<=a or a+c<=b
   return :equilateral if a==b and a==c
   return :isosceles if a==b or b==c or a==c
   :scalene
